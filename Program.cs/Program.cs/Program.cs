b31c0aaa3897aca5a5c4386be2d1b31432167926
adamr <adamrzetelski@gmail.com>
2023-02-19 15:07:43 +0100

    Zadanie domowe dzień 4

---
+++ Program.cs/Program.cs/Program.cs
@@ -0,0 +1,21 @@
+﻿char sex = 'F';
+int age = 25;
+
+if(sex == 'F' && age<30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+string name = "Ewa";
+int age1 = 33;
+if(name == "Ewa" && age1==33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+char sex1 = 'M';
+int age2 = 15;
+if(sex1 == 'M' && age2<18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
