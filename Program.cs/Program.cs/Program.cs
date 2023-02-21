de9891b40d8340c1c4555b545caf0f7417478b00
adamr <adamrzetelski@gmail.com>
2023-02-21 21:06:08 +0100

    Zadanie domowe 4 raz jeszcze

--- Program.cs/Program.cs/Program.cs
+++ Program.cs/Program.cs/Program.cs
@@ -1,25 +1,80 @@
-﻿// "F" - Female 
-// "M" - Male
-
-
-char sex = 'F';
-int age = 25;
-
-if(sex == 'F' && age<30)
-{
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-
+﻿int age = 17;
 string name = "Ewa";
-int age1 = 33;
-if(name == "Ewa" && age1==33)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
+string sex = "Female";
 
-char sex1 = 'M';
-int age2 = 15;
-if(sex1 == 'M' && age2<18)
+if (age > 0)
 {
-    Console.WriteLine("Niepełnoletni Mężczyzna");
+    if (age < 18)
+    {
+        if (sex == "Female" || sex == "female")
+        {
+            if (name == "Ewa" || name == "ewa")
+            {
+                Console.WriteLine("Ewa poniżej 18 lat");
+            }
+            else
+            {
+                Console.WriteLine("Jakaś inna kobieta niebędąca Ewą poniżej 18 lat");
+            }
+
+        }
+        else if (sex == "Male" || sex == "male")
+        {
+            if (name == "Jakub" || name == "jakub")
+            {
+                Console.WriteLine("Jakub poniżej 18 lat");
+            }
+            else
+            {
+                Console.WriteLine("Jakiś inny mężczyzna niebędący Jakubem poniżej 18 lat");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Czy jesteś osobą niebinarną?");
+        }
+    }
+    else if (age >= 18 && age < 150)
+    {
+        if (sex == "Female" || sex == "female")
+        {
+            if (name == "Ewa" || name == "ewa")
+            {
+                Console.WriteLine("Ewa pomiędzy 18 a 150 rokiem życia");
+            }
+            else
+            {
+                Console.WriteLine("Inna kobieta niebędąca Ewą pomiędz 18 a 150 rokiem życia");
+
+            }
+        }
+        else if (sex == "Male" || sex == "male")
+        {
+            if (name == "Jakub" || name == "Jakub")
+            {
+                Console.WriteLine("Jakub pomiędzy 18 a 150 rokiem życia");
+            }
+            else
+            {
+                Console.WriteLine("Inny mężczyzna pomiędzy 18 a 150 rokiem życia");
+
+            }
+        }
+        else
+        {
+            Console.WriteLine("Czy jesteś osobą niebinarną");
+        }
+        
+            
+        
+        
+    }
+    else
+    {
+        Console.WriteLine("Czy jesteś starożytnym mnichem czy wampirem");
+    }
+}
+else
+{
+    Console.WriteLine("Przykro mi, ale prawdopodobnie nie istniejesz albo dopiero się rodzisz");
 }
